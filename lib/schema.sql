b98bc30f24fd1f54c1627bc91e6cb8b56ffa2a3f
David Ritchey <ritchey0713@gmail.com>
2018-04-26 02:50:16 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE series (
     id INTEGER PRIMARY KEY,
     title TEXT,
     author_id TEXT,
-    subgenre_id TEXT
+    subgenre_id INTEGER
 );
 
 CREATE TABLE sub_genre (
@@ -18,5 +18,14 @@ CREATE TABLE books (
     id INTEGER PRIMARY KEY,
     title TEXT,
     year INTEGER,
-    series_id 
-)
+    series_id INTEGER
+); 
+
+CREATE TABLE characters (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    motto TEXT,
+    species TEXT,
+    author_id INTEGER,
+    series_id INTEGER
+);
