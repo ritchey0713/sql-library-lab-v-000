8c7e9686a65782e2c409724af72184074bfadf28
David Ritchey <ritchey0713@gmail.com>
2018-04-26 02:58:15 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,7 +1,7 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year FROM books 
    JOIN series 
-   ON books.series_id = books.id 
+   ON books.series_id = series.id 
    ORDER BY books.year"
 end
 
