d1eed0a2f5a3fe87f0c1e69a0fde30c3721fbdff
David Ritchey <ritchey0713@gmail.com>
2018-04-26 03:09:38 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,7 +20,7 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM authors 
   JOIN subgenres
-  on authors.id = subgenres_"
+  on authors.id = series.su"
 end
 
 def select_series_title_with_most_human_characters
