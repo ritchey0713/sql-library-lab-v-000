4f577a27d2382ac783068be4f67f06da6a73b531
David Ritchey <ritchey0713@gmail.com>
2018-04-26 03:03:10 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -2,19 +2,19 @@ def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year FROM books 
    JOIN series 
    ON books.series_id = series.id 
-   WHERE series.id = 1"
+   WHERE series.id = 1;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto FROM characters
-   ORDER BY characters.motto LIMIT 1"
+   ORDER BY characters.motto LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species COUNT (characters.species) FROM characters
    GROUP BY characters.species
-   ORDER BY characters.species DESC LIMIT 1"
+   ORDER BY characters.species DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
